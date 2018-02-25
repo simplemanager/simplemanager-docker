2501b78737271b3360367cf7f7eea8a847dc5b2a
Guillaume Ponçon <gponcon@gmail.com>
2018-02-25 20:54:27 +0100

    Main docker-compose builder.

---
+++ src/.build-compose.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cat docker-compose.yml \
+	| sed 's|image: simplemanager|image: simplemanager/simplemanager|' \
+        | sed 's|./files/|./src/files/|' \
+        | tr '\n' '\f' | sed 's/\f *build\: . *//' | tr '\f' '\n' > ../docker-compose.yml
+
